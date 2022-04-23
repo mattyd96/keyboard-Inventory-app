98777073d7226b6bddce82154010eaad96a777d4
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-04-23 16:02:06 +0930

    added server index.js and folders for development file structure

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require('apollo-server'); // apollo server for graphQL handling
+const mongoose = require('mongoose'); // mongoose for mondoDB
+
+//const typeDefs = require('./graphql/typeDefs'); // graphQL typedefs
+//const resolvers = require('./graphql/resolvers/index'); // graphQL resolvers
+
+const PORT = process.env.PORT || 4000; // get port from server provider or run locally
+
+
+const server = new ApolloServer({typeDefs, resolvers, context: ({ req }) => ({ req })});
+
+mongoose.connect(process.env.MONGODB, { useNewUrlParser: true })
+.then (() => {
+  return server.listen({port: PORT});
+})
+.then(res => {
+  console.log(`Server running at ${res.url}`);
+})
+.catch(err => {
+  console.error(err);
+});
