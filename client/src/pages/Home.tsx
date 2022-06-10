15e73e6bd31c79414e8da4126c80698be6582c52
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-06-10 22:33:48 +0930

    changed home => now redirects to inventory home when user is already logged in

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,20 +3,22 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import { AuthContext } from "../context/auth";
+import { Navigate } from "react-router-dom"
 
 function Home() {
   const { user } = useContext(AuthContext);
   return (
     <Layout>
-      <Stack align={'center'}>
-        <Title order={2}>
-          Welcome {user ? user.username: ''}
-        </Title>
-        {user ? 
-          <Button component={Link} to="/inventory/builds">Go to Builds</Button> :
+      {user ?
+        <Navigate replace to="/inventory/home"/>
+        :
+        <Stack align={'center'}>
+          <Title order={2}>
+            Welcome
+          </Title>
           <Button component={Link} to="/signup">Get Started</Button>
-        }
-      </Stack>
+        </Stack>
+      }
     </Layout>
   );
 }
